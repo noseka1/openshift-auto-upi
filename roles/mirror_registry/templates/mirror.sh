3499ab95f9dd64f66b66fe76a1a05a9944ae4336
Ales Nosek <ales.nosek@gmail.com>
2020-02-24 13:12:30 -0800

    Mirror registry for disconnected installs

---
+++ roles/mirror_registry/templates/mirror.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+oc adm release mirror \
+  --registry-config {{ helper.mirror_registry_dir }}/pull-secret.json \
+  --from={{ mirror_registry.mirror_from }} \
+  --to={{ mirror_registry.mirror_to }}
