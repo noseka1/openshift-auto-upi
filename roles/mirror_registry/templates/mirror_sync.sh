bdd30f149f597152af883a098f44ecc995fdec29
Ales Nosek <ales.nosek@gmail.com>
2020-08-07 07:12:50 -0700

    Update registry mirror to work with OCP 4.5.4

--- roles/mirror_registry/templates/mirror_sync.sh
+++ roles/mirror_registry/templates/mirror_sync.sh
@@ -2,4 +2,5 @@
 {{ helper.oc_exe }} adm release mirror \
   --registry-config {{ helper.mirror_registry_dir }}/pull-secret.json \
   --from {{ item.value.mirror_from }} \
-  --to {{ item.value.mirror_to }}
+  --to {{ item.value.mirror_to }} \
+  --to-release-image {{ item.value.mirror_to }}:{{ item.value.mirror_to_release }}
